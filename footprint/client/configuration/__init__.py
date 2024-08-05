983af04c21da564b1943f4d22d8ab22ac364c38c
Jason Kraus <zbyte64@gmail.com>
2024-08-05 14:39:31 -0700

    fixup model importing of client app

--- footprint/client/configuration/__init__.py
+++ footprint/client/configuration/__init__.py
@@ -1,4 +1,3 @@
-
 # UrbanFootprint v1.5
 # Copyright (C) 2017 Calthorpe Analytics
 #
@@ -10,18 +9,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
 # Public License v3 for more details; see <http://www.gnu.org/licenses/>.
 
-__author__ = 'calthorpe_analytics'
+__author__ = "calthorpe_analytics"
 
-# Import all client models that have static tables so that we have a single migration path
-from footprint.client.configuration.fixture import InitFixture
-from footprint.client.configuration.utils import resolve_fixture, resolve_client_module
-from django.conf import settings
 
-# Load all client modules into the system, even though we only will configure one CLIENT
-# This forces South to create all client specific table definitions
-for client in settings.ALL_CLIENTS:
-    client_init = resolve_fixture(None, "init", InitFixture, client)
-    #client_init.import_database()
-    for module_tuple in client_init.model_class_modules():
-        # Load the module so that Django and South find the classes
-        resolve_client_module(module_tuple[0], module_tuple[1], client)
+from footprint.client.configuration.default.layer_style import *
---
+++ footprint/client/configuration/models.py
@@ -0,0 +1,30 @@
+from footprint.client.configuration.default.layer_style.built_form_feature_layer_style import (
+    BuiltFormFeatureLayerStyle,
+)
+from footprint.client.configuration.default.layer_style.census_block_layer_style import (
+    CensusBlockLayerStyle,
+)
+from footprint.client.configuration.default.layer_style.census_blockgroup_layer_style import (
+    CensusBlockgroupLayerStyle,
+)
+from footprint.client.configuration.default.layer_style.census_tract_layer_style import (
+    CensusTractLayerStyle,
+)
+from footprint.client.configuration.default.layer_style.energy_feature_layer_style import (
+    EnergyFeatureLayerStyle,
+)
+from footprint.client.configuration.default.layer_style.feature_layer_style import (
+    FeatureLayerStyle,
+)
+from footprint.client.configuration.default.layer_style.layer_selection_layer_style import (
+    LayerSelectionSelectedLayerStyle,
+)
+from footprint.client.configuration.default.layer_style.ph_block_group_outcomes_feature_layer_style import (
+    PhBlockGroupOutcomesLayerStyle,
+)
+from footprint.client.configuration.default.layer_style.vmt_feature_layer_style import (
+    VmtFeatureLayerStyle,
+)
+from footprint.client.configuration.default.layer_style.water_feature_layer_style import (
+    WaterFeatureLayerStyle,
+)
